3c4018d95170ee00dd265d65e280d16bdba02d8a
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-02-23 10:33:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 class Config:
-    API_ID"=25639252"
+    API_ID="25639252"
     API_HASH="42db0fd56c51ff2b94cf064838eba7c1"
     TOKEN="6879955208:AAHjbpdm9Xxlv9gKJbtiUgDFDA5AGux2fwA"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "5975681177").split(" "))
