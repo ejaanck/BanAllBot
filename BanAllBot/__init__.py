01eac35b78bd7ebde410c672bf456a17fecf73d4
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-02-23 10:52:35 +0700

    Update __init__.py

--- BanAllBot/__init__.py
+++ BanAllBot/__init__.py
@@ -43,7 +43,7 @@ app=Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=TOKEN,
-    plugins=dict(BanAllBot.modules)
+    plugins=dict(root="BanAllBot.modules")
      )
 
 LOG.info("starting the bot....")
